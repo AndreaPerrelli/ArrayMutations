bf4532895057c9f1812e752978bfb086a24ff6dd
Andrea <Andrea@DESKTOP-BDOFMVL>
2022-04-19 02:03:52 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def mutate_array(array_to_mutate):
+    local_mutated_array = []
+    for element in array_to_mutate:
+        element = element * element
+        local_mutated_array.append(element)
+    return local_mutated_array
+
+
+def print_array(array):
+    for element in array:
+        print(element)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    array_to_mutate = [1, 2, 3, 4, 5, 6]
+    print_array(array_to_mutate)
+    mutated_array = mutate_array(array_to_mutate)
+    print_array(mutated_array)
+
